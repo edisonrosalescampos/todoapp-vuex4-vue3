fd10dda77dcc4c3f682e768625e2572e53667370
edison rosales <edisonrosalescampos@gmail.com>
2024-01-04 21:38:57 -0400

    subiendo cambios estilos

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -30,7 +30,7 @@ const changeTaskStatus = (id, completed) => {
     <div class="card-body">
       <div class="form-check">
         <input type="checkbox" class="form-check-input" :id="'chk' + task.id" @change="changeTaskStatus(task.id, !task.completed)">
-        <label class="form-check-label" :for="'chk' + task.id">
+        <label class="form-check-label me-2" :for="'chk' + task.id">
           <input type="text" class="form-control form-control-sm" v-model="task.description" @keyup.enter="saveTask(task)" v-if="editTaskFlag">
           <span v-else>
             {{ task.description }}
@@ -40,7 +40,7 @@ const changeTaskStatus = (id, completed) => {
         <span class="badge bg-danger px-1" v-else>pending</span>
       </div>
 
-      <div class="d-flex gap-1 ms-2">
+      <div class="d-flex gap-2 ms-2">
         <button type="button" class="btn btn-warning btn-sm" @click="editTask" v-if="!editTaskFlag"><i class="fas fa-pencil-alt"></i></button>
         <button type="button" class="btn btn-success btn-sm" @click="saveTask(task)" v-else><i class="fas fa-save"></i></button>
         <button type="button" class="btn btn-danger btn-sm" @click="deleteTask(task.id)"><i class="fa fa-trash"></i></button>
--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -7,8 +7,9 @@ defineProps({
 </script>
 
 <template>
-    <div v-if="tasks.length === 0"><p class="text-white text-center">No pending tasks to show</p></div>
+    <div v-if="tasks.length === 0"><p class="text-white text-center">No tasks to show</p></div>
     <div class="task-list" v-else>
+        <h6 class="title">TODO LIST</h6>
         <TaskItem v-for="(task, index) in tasks" :task="task" :key="task.id" />
     </div>
 </template>
